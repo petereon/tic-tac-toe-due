68236cfdfb22fb5defce648cae9899427511ac71
Peter Vyboch <peter.vyboch@ibm.com>
2022-04-04 18:56:28 +0200

    refactor: arguments to make functions more testable

--- src/tic_tac_toe_due/__init__.py
+++ src/tic_tac_toe_due/__init__.py
@@ -1,9 +1,6 @@
-import numpy as np
 import random
 from typing import List, Tuple, Union
 import time
-
-player_mapping = {"X": 1, "O": -1, 1: "X", -1: "O", " ": 0, 0: " "}
 
 
 def initialize_game():
@@ -16,92 +13,63 @@ def initialize_game():
     return state
 
 
-def generate_board():
-    return np.zeros((3, 3))
+def get_indexes(board: List[List[str]], value: str) -> List[Tuple[int, int]]:
+    indexes = []
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == value:
+                indexes.append((i, j))
+    return indexes
 
 
-def generate_board_repr(board: np.ndarray, endmes: Union[None, str]):
-    if (board == np.zeros((3, 3))).all():
-        print(
-            """Game Board Creation...
- | | 
--+-+-
- | | 
--+-+-
- | | 
-Board Created."""
-        )
-    else:
-        turn = len(np.argwhere(board != 0)) % 2
-
-        if turn == 1:
-            startmes = "Player X:\n"
-        else:
-            startmes = "Player O:\n"
-
-        # create board:
-        boardmes = ""
-        i = 0
-        for row in board.tolist():
-            i += 1
-            boardmes += "|".join([player_mapping[int(player)] for player in row]) + "\n"
-            if i != 3:
-                boardmes += "-+-+-\n"
-
-        print(startmes + boardmes + "\n\n" + (endmes if endmes is not None else ""))
+def get_random_move(game_state: dict) -> Tuple[int, int]:
+    empty_cell_indexes = get_indexes(game_state["board"], ' ')
+    return empty_cell_indexes[random.randint(0, len(empty_cell_indexes) - 1)]
 
 
-def player_sign(possible: list, player: str) -> int:
-    """
-    Give random possible move
-    """
-
-    rand_num = random.randint(0, len(possible) - 1)
-    return possible[rand_num]
-
-
-def assess_game(board: np.ndarray, position: int) -> int:
-    """
-    Assess the game
-    """
-    row, col = divmod(position - 1, 3)
-
-    player = board[row][col]
-    where_player = np.argwhere(board == player)
-
-    # Vertical
-    counter = 0
-    for i, j in where_player.tolist():
+def player_wins(game_state: dict, player: str) -> bool:
+    all_cells_taken_by_player = get_indexes(game_state["board"], player)
+    if len(all_cells_taken_by_player) < 3:
+        return False
+    cols, rows = list(zip(*all_cells_taken_by_player))
+    for i in range(3):
         # Vertical
-        if j == col:
-            counter += 1
-        if counter == 3:
-            return player
+        if cols.count(i) == 3:
+            return True
+        # Horizontal
+        if rows.count(i) == 3:
+            return True
 
-    # Horizontal
-    counter = 0
-    for i, j in where_player.tolist():
-        if i == row:
-            counter += 1
-        if counter == 3:
-            return player
+    # Diagonal dexter
+    if [abs(i-j) for i, j in all_cells_taken_by_player].count(0) == 3:
+        return True
+    
+    # Diagonal sinister
+    if [i+j for i, j in all_cells_taken_by_player].count(3) == 3:
+        return True
+    
 
-    # Diagonal
-    counter = 0
-    for i, j in where_player.tolist():
-        if i == j:
-            counter += 1
-        if counter == 3:
-            return player
-
-    if len(np.argwhere(board == 0)) == 0:
-        return 0
-    else:
-        return None
+    return False
 
 
-def process_move(game_state: dict, move: List[int]) -> Tuple[dict, bool]:
-    row, col = (move[0], move[1])
+def assess_game(game_state: dict) -> dict:
+    if player_wins(game_state, "X"):
+        game_state['status'] = "X"
+        game_state['msg'] = "PLAYER X WON!"
+    elif player_wins(game_state, "O"):
+        game_state['status'] = "O"
+        game_state['msg'] = "PLAYER O WON!"
+    elif len(get_indexes(game_state["board"], " ")) == 0:
+        game_state['status'] = "D"
+        game_state['msg'] = "GAME ENDS WITH A DRAW!"
+    
+    return game_state
+
+
+def process_move(game_state: dict, move_position: Union[Tuple[int, int], None] = None) -> Tuple[dict, bool]:
+    if move_position is None:
+        move_position = get_random_move(game_state)
+    row, col = (move_position[0], move_position[1])
     if (row >= 3) or (col >= 3):
         game_state["msg"] = "Invalid move, provide a valid position"
         return game_state, False
@@ -114,37 +82,26 @@ def process_move(game_state: dict, move: List[int]) -> Tuple[dict, bool]:
     return game_state, True
 
 
-def process_round(board: np.ndarray, player, turn_position):  # eg. pick square with id 7
-    row, col = divmod(turn_position - 1, 3)  # check
-    board[row][col] = player
-    game_status = assess_game(board, turn_position)
-    if game_status == 1:
-        endmes = "PLAYER X WON!"
-    elif game_status == -1:
-        endmes = "PLAYER O WON!"
-    elif game_status == 0:
-        endmes = "GAME ENDS WITH A DRAW!"
+def process_round(game_state, move_position: Union[Tuple[int, int], None] = None) -> dict:
+    game_state, success = process_move(game_state, move_position)
+    if success:
+        game_state = assess_game(game_state)
+    if game_state['player'] == 'X':
+        game_state['player'] = 'O'
     else:
-        endmes = None
-    generate_board_repr(board, endmes)
-    return board, game_status
+        game_state['player'] = 'X'
+    return game_state
+    
 
 
 def play_game(sleeptime=2):
     """Put together the game"""
+    game_state = initialize_game()
 
-    board = generate_board()
-    generate_board_repr(board, None)
 
-    player = 1
-    possible = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    game_status = None
-
-    while game_status is None:
-        turn_position = player_sign(possible, player_mapping[player])
-        possible.remove(turn_position)
-        board, game_status = process_round(board, player, turn_position)
-        player = player * (-1)
+    while game_state['status'] is None:
+        game_state = process_round(game_state)
+        print(game_state['board'])
         time.sleep(sleeptime)
 
 
