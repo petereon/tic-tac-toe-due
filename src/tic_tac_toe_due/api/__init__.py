b7d899695e3bfeb6b6f1b55b73a969600f3c4263
Peter Vyboch <peter.vyboch@ibm.com>
2022-04-05 23:30:57 +0200

    feat: finished API

--- src/tic_tac_toe_due/api/__init__.py
+++ src/tic_tac_toe_due/api/__init__.py
@@ -1,20 +1,54 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
+from typing import Tuple
+
+from uuid import uuid4
+
+from pydantic import BaseModel
 
 from tic_tac_toe_due import *
 
 app = FastAPI()
 
 
+class Move(BaseModel):
+    move: Tuple[int, int]
+
+
+games = {}
+
+
 @app.get("/game/new_game")
-def get_new_game():
-    pass
+async def get_new_game():
+    global games
+    state = initialize_game()
+    id = uuid4().hex
+    games[id] = state
+
+    return {"id": id, "state": state}
 
 
 @app.post("/game/{game_id}/move")
-def post_move(game_id):
-    pass
+async def post_move(game_id: str, move: Move, response: Response):
+    global games
+    state = games[game_id]
+    state, valid = process_round(state, move.move)
+    if not valid:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+    games[game_id] = state
+
+    return state
 
 
 @app.get("/game/{game_id}/state")
-def get_state(game_id):
-    pass
+async def get_state(game_id: str, response: Response):
+    global games
+    state = games.get(game_id, False)
+    if state == False:
+        state = {
+            "board": None,
+            "player": None,
+            "status": None,
+            "msg": "No such game",
+        }
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return state
